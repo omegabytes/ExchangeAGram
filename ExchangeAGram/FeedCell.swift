4791d7f64f3531f7d7ed2561a814aad6c060b011
Alex Gaesser <agaesser@gmail.com>
2015-01-24 15:33:20 -0500

    setup

---
+++ ExchangeAGram/FeedCell.swift
@@ -0,0 +1,13 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by Alex Gaesser on 1/24/15.
+//  Copyright (c) 2015 AlexGaesser. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+}
