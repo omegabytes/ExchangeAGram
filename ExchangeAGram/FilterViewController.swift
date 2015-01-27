e58f32d3d3ef7448a6acd25a05d753e4dd28ccc5
Alex Gaesser <agaesser@gmail.com>
2015-01-26 21:07:36 -0500

    right before GCD

--- ExchangeAGram/FilterViewController.swift
+++ ExchangeAGram/FilterViewController.swift
@@ -13,6 +13,12 @@ class FilterViewController: UIViewController, UICollectionViewDataSource, UIColl
     var thisFeedItem: FeedItem!
     
     var collectionView: UICollectionView!
+    
+    let kIntensity = 0.7
+    
+    var context:CIContext = CIContext(options: nil)
+    
+    var filters:[CIFilter] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,7 +37,7 @@ class FilterViewController: UIViewController, UICollectionViewDataSource, UIColl
         
         self.view.addSubview(collectionView)
         
-        
+        filters = photoFilters()
         
     }
 
@@ -40,18 +46,73 @@ class FilterViewController: UIViewController, UICollectionViewDataSource, UIColl
         // Dispose of any resources that can be recreated.
     }
     
+    // UICollectionViewDataSource
+    
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+        return filters.count
     }
 
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         
         let cell:FilterCell = collectionView.dequeueReusableCellWithReuseIdentifier("MyCell", forIndexPath: indexPath) as FilterCell
         
-        cell.imageView.image = UIImage(named: "Placeholder")
+//        cell.imageView.image = UIImage(named: "Placeholder")
+        
+        cell.imageView.image = filteredImageFromImage(thisFeedItem.image, filter: filters[indexPath.row])
         
         return cell
     }
+    
+    // Helper function
+    
+    
+    func photoFilters () -> [CIFilter] {
+        
+        let blur = CIFilter(name: "CIGaussianBlur")
+        let instant = CIFilter(name: "CIPhotoEffectInstant")
+        let noir = CIFilter(name: "CIPhotoEffectNoir")
+        let transfer = CIFilter(name: "CIPhotoEffectTransfer")
+        let unsharpen = CIFilter(name: "CIUnsharpMask")
+        let monochrome = CIFilter(name: "CIColorMonochrome")
+        
+        let colorControls = CIFilter(name: "CIColorControls")
+        colorControls.setValue(0.5, forKey: kCIInputSaturationKey)
+        
+        let sepia = CIFilter(name: "CISepiaTone")
+        sepia.setValue(kIntensity, forKey: kCIInputIntensityKey)
+        
+        let colorClamp = CIFilter(name: "CIColorClamp")
+        colorClamp.setValue(CIVector(x: 0.9, y: 0.9, z: 0.9, w: 0.9), forKey: "inputMaxComponents")
+        colorClamp.setValue(CIVector(x: 0.2, y: 0.2, z: 0.2, w: 0.2), forKey: "inputMinComponents")
+        
+        let composite = CIFilter(name: "CIHardLightBlendMode")
+        composite.setValue(sepia.outputImage, forKey: kCIInputImageKey)
+        
+        let vignette = CIFilter(name: "CIVignette")
+        vignette.setValue(composite.outputImage, forKey: kCIInputImageKey)
+        
+        vignette.setValue(kIntensity * 2, forKey: kCIInputIntensityKey)
+        vignette.setValue(kIntensity * 30, forKey: kCIInputRadiusKey)
+        
+        
+        return [blur, instant, noir, transfer, unsharpen, monochrome, colorControls, sepia, colorClamp, composite, vignette]
+    }
+    
+    func filteredImageFromImage (imageData: NSData, filter: CIFilter) -> UIImage {
+        
+        let unfilteredImage = CIImage(data: imageData)
+        filter.setValue(unfilteredImage, forKey: kCIInputImageKey)
+        let filteredImage:CIImage = filter.outputImage
+        
+        let extent = filteredImage.extent()
+        let cgImage:CGImageRef = context.createCGImage(filteredImage, fromRect: extent)
+        
+        let finalImage = UIImage(CGImage: cgImage)
+        
+        return finalImage!
+    }
+    
+    
     
     
     
